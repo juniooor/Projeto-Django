3b1434c71c9dfced200c5a10579f1e5a78a14d6f
Juniooor <juniormodern@hotmail.com>
2022-11-28 21:18:26 -0300

    hhtp request 2

--- project/urls.py
+++ project/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 
-
-def my_view(request):
-    return HttpResponse('Hello world')
-
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view)
+    path('', home)
 ]
