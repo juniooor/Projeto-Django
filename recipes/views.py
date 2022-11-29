d4365dbec23d03361912aeec516c03de80291afc
Juniooor <juniormodern@hotmail.com>
2022-11-29 20:47:00 -0300

    organizando urls 1/?

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('Hello world')
 # Create your views here.
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
