c2d6ff5bb47d0edcfe02ca309543d7c0e6226f4f
Juniooor <juniormodern@hotmail.com>
2022-11-29 21:50:38 -0300

    organizando urls 2/?

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Hello world')
+    return render(request, 'recipes/home.html')
 # Create your views here.
 
 
