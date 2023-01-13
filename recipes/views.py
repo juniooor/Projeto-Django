0a4e433abec298480926ac2e9f4994d11499c749
Juniooor <juniormodern@hotmail.com>
2023-01-12 22:29:17 -0300

    organizando category´s

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,7 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -15,9 +15,10 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
 
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
