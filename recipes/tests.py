a4775b3ca0c2fa6d3066185c92e45bb393f59658
Juniooor <juniormodern@hotmail.com>
2023-01-20 21:23:04 -0300

    iniciando test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURlsTest(TestCase):
+    def test_testing_pytest(self):
+        print("Hello errors")
+        assert 1 == 1
