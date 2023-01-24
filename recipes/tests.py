92eb08264bb649cbbaa7928f66592d3ef935f936
Juniooor <juniormodern@hotmail.com>
2023-01-23 21:11:45 -0300

    testando as urls e aprendendo testes unitarios

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,3 +6,11 @@ class RecipeURlsTest(TestCase):
     def test_recipe_home_is_ok(self):
         home_url = reverse('recipes:home')
         self.assertEqual(home_url, '/')
+
+    def test_recipe_category_is_ok(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_is_ok(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
