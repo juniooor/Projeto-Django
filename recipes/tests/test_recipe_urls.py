718bf2e66ff17463c13c64e5fd5f321c20516281
Juniooor <juniormodern@hotmail.com>
2023-01-26 20:25:35 -0300

    testes 2

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+from django.urls import reverse
+
+
+class RecipeURlsTest(TestCase):
+    def test_recipe_home_is_ok(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+    def test_recipe_category_is_ok(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_is_ok(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
---
+++ recipes/tests/test_recipe_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+from recipes import views
+
+
+class RecipeViewsTest(TestCase):
+
+    def test_recipe_home_views_is_ok(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_views_is_ok(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_views_is_ok(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
