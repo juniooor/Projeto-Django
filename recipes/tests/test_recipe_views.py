059d0a676f6bedf9d798ad39a6fd7c51e42a4fde
Juniooor <juniormodern@hotmail.com>
2023-01-31 21:29:11 -0300

    testando status 404 recipe/category

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -5,19 +5,6 @@ from recipes import views
 
 
 class RecipeViewsTest(TestCase):
-
-    def test_recipe_home_views_is_ok(self):
-        view = resolve(reverse('recipes:home'))
-        self.assertIs(view.func, views.home)
-
-    def test_recipe_category_views_is_ok(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertIs(view.func, views.category)
-
-    def test_recipe_detail_views_is_ok(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
-
     def test_recipe_home_view_return_status_200(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
@@ -32,3 +19,26 @@ class RecipeViewsTest(TestCase):
             'No recipes found',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_views_is_ok(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_views_is_ok(self):
+        view = resolve(reverse('recipes:category',
+                       kwargs={'category_id': 1000}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_views_is_ok(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000}))
+        self.assertEqual(response.status_code, 404)
